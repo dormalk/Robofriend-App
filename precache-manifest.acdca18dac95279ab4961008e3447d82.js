c5228af50d0732c2c0239733dc1bda6c4f4a6efc
Dor <dorm@service-wise.com>
2019-09-02 14:01:18 +0300

    Updates

---
+++ precache-manifest.acdca18dac95279ab4961008e3447d82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd2fc618d82251d45888e5b38d153227",
+    "url": "/dormalk/Robofriend-App/index.html"
+  },
+  {
+    "revision": "49d335addeb602331e35",
+    "url": "/dormalk/Robofriend-App/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e07d8dbafa47be7c3e62",
+    "url": "/dormalk/Robofriend-App/static/css/main.95e11c59.chunk.css"
+  },
+  {
+    "revision": "49d335addeb602331e35",
+    "url": "/dormalk/Robofriend-App/static/js/2.dffb7b9e.chunk.js"
+  },
+  {
+    "revision": "e07d8dbafa47be7c3e62",
+    "url": "/dormalk/Robofriend-App/static/js/main.02f65487.chunk.js"
+  },
+  {
+    "revision": "502c965d070f558d3877",
+    "url": "/dormalk/Robofriend-App/static/js/runtime~main.4b6e32ef.js"
+  }
+]);
